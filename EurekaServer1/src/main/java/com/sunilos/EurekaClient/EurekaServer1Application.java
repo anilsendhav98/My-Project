82e23427f8930f425fe593846ece1d0753747346
anilsendhav98 <anil.sendhav98@gmail.com>
2020-12-08 14:11:55 +0530

    EurekaServer Anil

--- EurekaServer1/src/main/java/com/sunilos/EurekaClient/EurekaServer1Application.java
+++ EurekaServer1/src/main/java/com/sunilos/EurekaClient/EurekaServer1Application.java
@@ -3,7 +3,7 @@ package com.sunilos.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//This is change by Anil
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer1Application {
