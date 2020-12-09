cb223acce3847b7ca113f2d229e3c8f113235fca
anilsendhav98 <anil.sendhav98@gmail.com>
2020-12-09 19:17:23 +0530

    anil client Change

--- EurekaClient/src/main/java/com/sunilos/EurekaClient/EurekaClientApplication.java
+++ EurekaClient/src/main/java/com/sunilos/EurekaClient/EurekaClientApplication.java
@@ -3,7 +3,7 @@ package com.sunilos.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//Anil Change
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClientApplication {
